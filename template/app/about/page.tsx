9d77ebf94eac52872fa4ad7d78cb389ea89d79fb
Jonasppxx <Jonas.boos@bwdbern.ch>
2025-11-05 19:41:46 +0100

    update

---
+++ template/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>Über uns</h1>
+      <p>Dies ist eine Beispiel-Unterseite.</p>
+    </div>
+  );
+}
---
+++ template/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Willkommen zu meiner Next.js Vorlage!</h1>
+        <p>Dies ist die Hauptseite.</p>
+      </main>
+    </div>
+  );
+}
